1e1fc637c2cc4ce3f1aa73dc282de22c0ac8cde9
Guilherme Santos <guisantossilva00@gmail.com>
2023-01-31 12:34:05 -0300

    Feito Header do Login

---
+++ force-app/main/default/lwc/headerLogin/headerLogin.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import ShopPetz from '@salesforce/resourceUrl/ImagensShopPetz';
+
+
+export default class HeaderLogin extends LightningElement {
+    arquivos = {
+        logo: `${ShopPetz}/logoShopPetz.png`
+    }
+}
