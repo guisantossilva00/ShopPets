077c5d950e0f48d175e0a916c1f903e12ec1f119
Guilherme Santos <guisantossilva00@gmail.com>
2023-02-13 16:22:08 -0300

    Finalizado as validações com javascript

--- force-app/main/default/lwc/cadastroPet/cadastroPet.js
+++ force-app/main/default/lwc/cadastroPet/cadastroPet.js
@@ -8,6 +8,13 @@ export default class CadastroPet extends LightningElement {
     setaBaixoIcon = setaBaixoPng;
     calendarioIcon = calendarioPng;
 
+    nomeDoPet = '';
+    generoDoPet = '';
+    anoNascimentoPet;
+
+    formulario;
+    // elementoQualPet;
+
     @track qualTipoPet = {
         Cachorro : '',
         Gato : '',
@@ -15,12 +22,16 @@ export default class CadastroPet extends LightningElement {
         Passaro : '',
     }
 
-    nomeDoPet = '';
-    generoDoPet = '';
     renderedCallback() {
-        
+        this.formulario = this.template.querySelector('form');
+            
+        this.formulario.addEventListener('submit', (event) => {
+            event.preventDefault();
+            
+            this.validaForm();
+        })
     }
-
+    
     getNames(event) {
         const campoName = event.target.name;
         
@@ -28,14 +39,12 @@ export default class CadastroPet extends LightningElement {
             if(event.target.value == 'Cachorro') {
                 this.qualTipoPet.Cachorro = true;
 
-                console.log('this.qualTipoPet.Cachorro => ' + this.qualTipoPet.Cachorro);
                 this.qualTipoPet.Gato = false;   
                 this.qualTipoPet.Roedor = false;
                 this.qualTipoPet.Passaro = false;
             }
             else if (event.target.value == 'Gato') {
                 this.qualTipoPet.Gato = event.target.value;  
-                console.log('this.qualTipoPet.Gato => ' + this.qualTipoPet.Gato);
 
                 this.qualTipoPet.Cachorro = false;
                 this.qualTipoPet.Roedor = false;
@@ -43,7 +52,6 @@ export default class CadastroPet extends LightningElement {
             }
             else if (event.target.value == 'Roedor') {
                 this.qualTipoPet.Roedor = event.target.value;
-                console.log('this.qualTipoPet.Roedor => ' + this.qualTipoPet.Roedor);
 
                 this.qualTipoPet.Cachorro = false;
                 this.qualTipoPet.Gato = false;   
@@ -51,7 +59,6 @@ export default class CadastroPet extends LightningElement {
             }
             else if (event.target.value == 'Passaro') {
                 this.qualTipoPet.Passaro = event.target.value;
-                console.log('this.qualTipoPet.Passaro => ' + this.qualTipoPet.Passaro);
 
                 this.qualTipoPet.Cachorro = false;
                 this.qualTipoPet.Gato = false;   
@@ -64,6 +71,93 @@ export default class CadastroPet extends LightningElement {
         else if (campoName == 'generoPet') {
             if(event.target.value == 'Fêmea') this.generoDoPet = event.target.value;
             else this.generoDoPet = false;
+        } 
+        else if(campoName == 'anoNascimentoPet') {
+            const dataAtual = new Date();
+            const anoAtual = parseInt(dataAtual.getFullYear());
+            const idadePet = parseInt(event.target.value);
+
+            const dataNascimento = anoAtual - idadePet + '-01-01';
+            
+            this.anoNascimentoPet = dataNascimento;
         }
+    }
+
+    validaForm() {
+        const elementoQualPet = this.template.querySelector('[name="qualPet"]');
+        const elementoNomePet = this.template.querySelector('[name="nomePet"]');
+        const elementoGeneroPet = this.template.querySelector('[name="generoPet"]');
+        const elementoRacaPet = this.template.querySelector('[name="racaPet"]');
+        const elementoDataNascimentoPet = this.template.querySelector('[name="dataNascimentoPet"]');
+        const elementoCastradoPet = this.template.querySelector('[name="castradoPet"]');
+        const elementoPesoPet = this.template.querySelector('[name="pesoPet"]');
+        const elementoTemperamentoPet = this.template.querySelector('[name="temperamentoPet"]');
+
+        if (elementoQualPet.value == '') {
+            this.setErro(elementoQualPet, 'O tipo do pet é obrigatório');
+        } else {
+            this.setSucesso(elementoQualPet);
+        }
+        
+        if (elementoNomePet.value == '') {
+            this.setErro(elementoNomePet, 'O nome do pet é obrigatório');
+        } else {
+            this.setSucesso(elementoNomePet);
+        }
+
+        if (elementoGeneroPet.value == '') {
+            this.setErro(elementoGeneroPet, 'O gênero do pet é obrigatório');
+        } else {
+            this.setSucesso(elementoGeneroPet);
+        }
+
+        if (elementoRacaPet.value == '') {
+            this.setErro(elementoRacaPet, 'A raça do pet é obrigatório');
+        } else {
+            this.setSucesso(elementoRacaPet);
+        }
+
+        if (elementoDataNascimentoPet.value == '') {
+            this.setErro(elementoDataNascimentoPet, 'A data de nascimento do pet é obrigatório');
+        } else {
+            this.setSucesso(elementoDataNascimentoPet);
+        }
+
+        if (elementoCastradoPet.value == '') {
+            this.setErro(elementoCastradoPet, 'O campo se o pet é castrado é obrigatório');
+        } else {
+            this.setSucesso(elementoCastradoPet);
+        }
+
+        if (elementoPesoPet.value == '') {
+            this.setErro(elementoPesoPet, 'O peso do pet é obrigatório');
+        } else {
+            this.setSucesso(elementoPesoPet);
+        }
+
+        if (elementoTemperamentoPet.value == '') {
+            this.setErro(elementoTemperamentoPet, 'O temperamento do pet é obrigatório');
+        } else {
+            this.setSucesso(elementoTemperamentoPet);
+        }
+
+    }
+
+    setErro(input, mensagem) {
+        const positionControl = input.parentElement;
+        const small = positionControl.querySelector('small');
+
+        small.innerText = mensagem;
+
+        positionControl.classList.add('erro');
+        positionControl.classList.remove('sucesso');
+        input.focus();
+    }
+
+    setSucesso(input) {
+        const positionControl = input.parentElement;
+
+        positionControl.classList.add('sucesso');
+        positionControl.classList.remove('erro');
     }
 }
